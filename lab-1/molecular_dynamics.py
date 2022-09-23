9e37ea045ef33c5c468c219a65ab949c4441f279
Johan Carlsen <johancarlsen@eduroam-193-157-185-156.wlan.uio.no>
2022-09-23 12:04:59 +0200

    Added some code to molecular_dynamics.py to get keywords

--- lab-1/molecular_dynamics.py
+++ lab-1/molecular_dynamics.py
@@ -3,11 +3,24 @@ import lammps_logfile
 import matplotlib.pyplot as plt
 
 log = lammps_logfile.File("log.lammps")
+keyWords = log.get_keywords()
 
-step = log.get("Step")
-pressure = log.get("Press")
-temperature = log.get("Temp")
-totEnergy = log.get("TotEng")
+# print(keyWords)
+'''['E_mol', 'E_pair', 'Press', 'Step', 'Temp', 'TotEng']'''
 
-plt.plot(step, temperature)
+E_mol = log.get('E_mol')
+E_pair = log.get('E_pair')
+press = log.get('Press')
+step = log.get('Step')
+temp = log.get('Temp')
+totEng = log.get('TotEng')
+
+avgE_mol = lammps_logfile.running_mean(E_mol, 100)
+avgE_pair = lammps_logfile.running_mean(E_pair, 100)
+avgPress = lammps_logfile.running_mean(press, 100)
+avgTemp = lammps_logfile.running_mean(temp, 100)
+avgTotEng = lammps_logfile.running_mean(totEng, 100)
+
+plt.plot(step, temp)
+plt.plot(step, avgTemp)
 plt.show()
