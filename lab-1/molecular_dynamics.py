2cb66fb93f9168de17f283c03da5df61f6d179db
JohanCarlsen <johan_carlsen@hotmail.com>
2022-09-27 21:58:48 +0200

    Now, all the info from log.lammps_ORIGINAL will be shown in a grid plot

--- lab-1/molecular_dynamics.py
+++ lab-1/molecular_dynamics.py
@@ -1,5 +1,6 @@
 # We want to extract the thermodynamic data from the file log.lammps
 import lammps_logfile
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -9,25 +10,57 @@ keyWords = log.get_keywords()
 # print(keyWords)
 '''['E_mol', 'E_pair', 'Press', 'Step', 'Temp', 'TotEng']'''
 
-E_mol = log.get('E_mol')
-E_pair = log.get('E_pair')
-press = log.get('Press')
-step = log.get('Step')
-temp = log.get('Temp')
-totEng = log.get('TotEng')
+_E_mol = log.get('E_mol')
+_E_pair = log.get('E_pair')
+_P = log.get('Press')
+_step = log.get('Step')
+_T = log.get('Temp')
+_E_tot = log.get('TotEng')
+
+E_mol = _E_mol[6:]
+E_pair = _E_pair[6:]
+P = _P[6:]
+t = _step[6:] - 101000
+T = _T[6:]
+E_tot = _E_tot[6:]
 
 avgE_mol = lammps_logfile.running_mean(E_mol, 100)
 avgE_pair = lammps_logfile.running_mean(E_pair, 100)
-avgPress = lammps_logfile.running_mean(press, 100)
-avgTemp = lammps_logfile.running_mean(temp, 100)
-avgTotEng = lammps_logfile.running_mean(totEng, 100)
+avgPress = lammps_logfile.running_mean(P, 100)
+avgTemp = lammps_logfile.running_mean(T, 100)
+avgTotEng = lammps_logfile.running_mean(E_tot, 100)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(11, 7), sharex=True)
-ax1.plot(step, totEng)
-ax1.set_yscale('log')
+fig = plt.figure(figsize=(11, 6.5))
+ax1 = plt.subplot2grid((2, 6), (0, 0), colspan=2)
+ax2 = plt.subplot2grid((2, 6), (0, 2), colspan=2)
+ax3 = plt.subplot2grid((2, 6), (0, 4), colspan=2)
+ax4 = plt.subplot2grid((2, 6), (1, 1), colspan=2)
+ax5 = plt.subplot2grid((2, 6), (1, 3) , colspan=2)
 
-ax2.plot(step, avgTotEng)
-ax2.set_yscale('log')
-ax2.set_xlabel('Time [$t$]')
+ax1.plot(t, E_mol, color='k', lw=.8)
+ax1.set_title('$E_{mol}$')
+ax1.set_xlabel('$t$')
+ax1.set_ylabel('$E$')
 
+ax2.plot(t, E_pair, color='k', lw=.8)
+ax2.set_title('$E_{pair}$')
+ax2.set_xlabel('$t$')
+ax2.set_ylabel('$E$')
+
+ax3.plot(t, P, color='k', lw=.8)
+ax3.set_title('Pressure')
+ax3.set_xlabel('$t$')
+ax3.set_ylabel('$P$')
+
+ax4.plot(t, T, color='k', lw=.8)
+ax4.set_title('Temperature')
+ax4.set_xlabel('$t$')
+ax4.set_ylabel('$T$')
+
+ax5.plot(t, E_tot, color='k', lw=.8)
+ax5.set_title('Total energy')
+ax5.set_xlabel('$t$')
+ax5.set_ylabel('$E$')
+
+plt.tight_layout()
 plt.show()
